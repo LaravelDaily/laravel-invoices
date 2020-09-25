e2f3345318272464756d4697726b86fb6c59e135
Marius Kasparavičius <marius.kasparavicius@mif.stud.vu.lt>
2020-09-25 09:46:11 +0300

    Exported purged CSS file to template

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -4,18 +4,110 @@
         <title>{{ $invoice->name }}</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-        <link rel="stylesheet" href="{{ asset('vendor/invoices/bootstrap.min.css') }}">
-
         <style type="text/css" media="screen">
-            * {
-                font-family: "DejaVu Sans";
-            }
             html {
+                font-family: sans-serif;
+                line-height: 1.15;
                 margin: 0;
             }
+
             body {
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+                font-weight: 400;
+                line-height: 1.5;
+                color: #212529;
+                text-align: left;
+                background-color: #fff;
                 font-size: 10px;
                 margin: 36pt;
+            }
+
+            h4 {
+                margin-top: 0;
+                margin-bottom: 0.5rem;
+            }
+
+            p {
+                margin-top: 0;
+                margin-bottom: 1rem;
+            }
+
+            strong {
+                font-weight: bolder;
+            }
+
+            img {
+                vertical-align: middle;
+                border-style: none;
+            }
+
+            table {
+                border-collapse: collapse;
+            }
+
+            th {
+                text-align: inherit;
+            }
+
+            h4, .h4 {
+                margin-bottom: 0.5rem;
+                font-weight: 500;
+                line-height: 1.2;
+            }
+
+            h4, .h4 {
+                font-size: 1.5rem;
+            }
+
+            .table {
+                width: 100%;
+                margin-bottom: 1rem;
+                color: #212529;
+            }
+
+            .table th,
+            .table td {
+                padding: 0.75rem;
+                vertical-align: top;
+                border-top: 1px solid #dee2e6;
+            }
+
+            .table thead th {
+                vertical-align: bottom;
+                border-bottom: 2px solid #dee2e6;
+            }
+
+            .table tbody + tbody {
+                border-top: 2px solid #dee2e6;
+            }
+
+            .mt-5 {
+                margin-top: 3rem !important;
+            }
+
+            .pr-0,
+            .px-0 {
+                padding-right: 0 !important;
+            }
+
+            .pl-0,
+            .px-0 {
+                padding-left: 0 !important;
+            }
+
+            .text-right {
+                text-align: right !important;
+            }
+
+            .text-center {
+                text-align: center !important;
+            }
+
+            .text-uppercase {
+                text-transform: uppercase !important;
+            }
+            * {
+                font-family: "DejaVu Sans";
             }
             body, h1, h2, h3, h4, h5, h6, table, th, tr, td, p, div {
                 line-height: 1.1;
