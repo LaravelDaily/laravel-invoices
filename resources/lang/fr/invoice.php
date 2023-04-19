7f20d64886a64150957de71d473e03ef02d17e0e
Luca ALSINA <contact@alsinaluca.fr>
2023-04-19 16:30:48 +0200

    Added french translation

---
+++ resources/lang/fr/invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Facture',
+    'serial'                 => 'Numéro de série',
+    'date'                   => 'Date de facturation',
+    'seller'                 => 'Vendeur',
+    'buyer'                  => 'Acheteur',
+    'address'                => 'Adresse',
+    'code'                   => 'Code',
+    'vat'                    => 'Code TVA',
+    'phone'                  => 'Téléphone',
+    'description'            => 'Description',
+    'units'                  => 'Unités',
+    'quantity'               => 'Qté',
+    'price'                  => 'Prix',
+    'discount'               => 'Remise',
+    'tax'                    => 'Taxe',
+    'sub_total'              => 'Sous-total',
+    'total_discount'         => 'Remise totale',
+    'taxable_amount'         => 'Montant taxable',
+    'total_taxes'            => 'Taxes totales',
+    'tax_rate'               => 'Taux de taxe',
+    'total_amount'           => 'Montant total',
+    'pay_until'              => 'Veuillez payer avant le',
+    'amount_in_words'        => 'Montant en lettres',
+    'amount_in_words_format' => '%s %s et %s %s',
+    'notes'                  => 'Notes',
+    'shipping'               => 'Livraison',
+    'paid'                   => 'Payé',
+    'due'                    => 'Dû',
+];
