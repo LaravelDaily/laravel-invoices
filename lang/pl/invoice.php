02861ac7f687fa53c32314cb16d652698bafba0d
Olsza <olsza@users.noreply.github.com>
2024-06-25 09:32:19 +0200

    add translate PL

---
+++ lang/pl/invoice.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | English Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Faktura',
+    'serial'                 => 'Nr seryjny',
+    'date'                   => 'Data faktury',
+    'seller'                 => 'Sprzedawca',
+    'buyer'                  => 'Buyer',
+    'address'                => 'Adres',
+    'code'                   => 'Kod',
+    'vat'                    => 'VAT',
+    'phone'                  => 'Telefon',
+    'description'            => 'Opis',
+    'units'                  => 'Jednostki',
+    'quantity'               => 'Ilość',
+    'price'                  => 'Cena',
+    'discount'               => 'Rabat',
+    'tax'                    => 'Podatek',
+    'sub_total'              => 'Razem',
+    'total_discount'         => 'Całkowity rabat',
+    'taxable_amount'         => 'Wartość opodatkowania',
+    'total_taxes'            => 'Suma podatków',
+    'tax_rate'               => 'Stawka podatku',
+    'total_amount'           => 'Całkowita kwota',
+    'pay_until'              => 'Płatność do',
+    'amount_in_words'        => 'Kwota słownie',
+    'amount_in_words_format' => '%s %s i %s %s',
+    'notes'                  => 'Uwagi',
+    'shipping'               => 'Wysyłka',
+    'paid'                   => 'Zapłacono',
+    'due'                    => 'Termin płatności',
+    'page'                   => 'Strona',
+];
