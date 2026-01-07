7fcf62094d0b82224d6ec383cc4b2abb69de34ea
David Lun <mysticcode@gmail.com>
2026-01-07 09:50:49 +0200

    Updates default logOutputFile value

--- config/invoices.php
+++ config/invoices.php
@@ -108,8 +108,9 @@ return [
         'enable_php' => true,
         /**
          * Do not write log.html or make it optional
+         *
          *  @see https://github.com/dompdf/dompdf/issues/2810
          */
-        'logOutputFile' => '/dev/null',
+        'logOutputFile' => '',
     ],
 ];
